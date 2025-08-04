624fa0954610204bd65ee6dca337e58a57cb12b1
RiiikkiTikkiTavi <https://github.com/RiiikkiTikkiTavi>
2025-08-04 18:19:18 +0300

    Добавлено логгирование с помощью talker

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
+import 'dart:async';
+
 import 'package:cookbook/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final talker = TalkerFlutter.init();
+  // регистрация в getIt
+  GetIt.I.registerSingleton(talker);
+
+  // обработка ошибок интерфейса
+  FlutterError.onError =
+      (details) => GetIt.I<Talker>().handle(details.exception, details.stack);
+
+  // дополнительная обработка ошибок
+  runZonedGuarded(() => runApp(const MyApp()), (e, st) {
+    GetIt.I<Talker>().handle(e, st);
+  });
 }
